43052a48a33d74adc7817e2f60a1673b0eeaf503
Kamen Kanev <kamenkanev88@gmail.com>
2024-01-21 14:43:46 +0200

    Update listModel.ts to include styles property

--- models/listModel.ts
+++ models/listModel.ts
@@ -1,15 +1,19 @@
-import { Schema, model, Document, Types } from "mongoose";
+import { Schema, model, Document, Types } from 'mongoose';
 
 interface IList extends Document {
     name: string;
     board: Types.ObjectId;
     cards: Types.ObjectId[];
+    styles: {
+        backgroundColor: string;
+    };
 }
 
 const listSchema = new Schema<IList>({
     name: { type: String, required: true },
     board: { type: Schema.Types.ObjectId, ref: 'Board', required: true },
     cards: [{ type: Schema.Types.ObjectId, ref: 'Card' }],
+    styles: { type: Schema.Types.Mixed },
 });
 
 const List = model<IList>('List', listSchema);
