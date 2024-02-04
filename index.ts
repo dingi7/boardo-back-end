79f0451d51a7abc078bf48c4da8d77e654e5fa6e
Kamen Kanev <kamenkanev88@gmail.com>
2024-02-04 13:57:31 +0200

    started adding sockets support

--- index.ts
+++ index.ts
@@ -1,70 +1,97 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-import { prettyJSON } from 'hono/pretty-json';
-import { logger } from 'hono/logger';
-import mongoose from 'mongoose';
-import { authHeader } from './middlewares';
-import { cors } from 'hono/cors'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+import { logger } from "hono/logger";
+import mongoose from "mongoose";
+import { authHeader } from "./middlewares";
+import { cors } from "hono/cors";
+import { Server } from "socket.io";
 
-import api from './api';
-import dotenv from 'dotenv';
+import api from "./api";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 async function connectToDatabase() {
-    const databaseUrl: string = process.env.MONGODB_URI || 'mongodb+srv://vercel-admin-user-65a6cd4b508d533ab52f834c:dSRHUBruefe8nOlB@boardocluster.ysfd69e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-    try {
-        await mongoose.connect(databaseUrl);
-        console.log('Connected to the database');
-    } catch (error) {
-        console.error('Database connection failed:', error);
-        process.exit(1);
-    }
+  const databaseUrl: string =
+    process.env.MONGODB_URI ||
+    "mongodb+srv://vercel-admin-user-65a6cd4b508d533ab52f834c:dSRHUBruefe8nOlB@boardocluster.ysfd69e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  try {
+    await mongoose.connect(databaseUrl);
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    process.exit(1);
+  }
 }
 
 function configureServer() {
-    const app = new Hono();
-    
-    // Middleware
-    app.use('*', logger());
-    app.use(authHeader);
-    app.use(prettyJSON());
+  const app = new Hono();
 
-    // cors
-    app.use(cors());
+  // Middleware
+  app.use("*", logger());
+  app.use(authHeader);
+  app.use(prettyJSON());
 
-    // Routes
-    app.get('/', (c) => c.text('Hello Hono!'));
-    app.route('/api/v1', api);
+  // cors
+  app.use(cors());
 
-    // Error Handling
-    app.notFound((c) => {
-        return c.json({ error: `🔍 - Not Found - ${c.req.url}` }, 404);
-    });
+  // Routes
+  app.get("/", (c) => c.text("Hello Hono!"));
+  app.route("/api/v1", api);
 
-    app.onError((err, c) => {
-        return c.json(
-            {
-                message: err.message,
-                stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
-            },
-            500
-        );
-    });
+  // Error Handling
+  app.notFound((c) => {
+    return c.json({ error: `🔍 - Not Found - ${c.req.url}` }, 404);
+  });
 
-    return app;
+  app.onError((err, c) => {
+    return c.json(
+      {
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+      },
+      500,
+    );
+  });
+
+  return app;
 }
 
 function startServer(app: Hono) {
-    serve(app, () => {
-        console.log('Server is running on port 3000');
-    });
+  serve(app, () => {
+    console.log("Server is running on port 3000");
+  });
 }
 
 async function start() {
-    await connectToDatabase();
-    const app = configureServer();
-    startServer(app);
+  await connectToDatabase();
+  const app = configureServer();
+  startServer(app);
+  const io = new Server(3009, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket: any) => {
+    socket.on("create-card", (data: any, boardId: string) => {
+      socket.to(boardId).emit("card-created", data);
+    });
+
+    socket.on("delete-card", (cardId: string, boardId: string) => {
+      socket.to(boardId).emit("card-deleted", cardId);
+    });
+
+    socket.on("create-list", (list: any, boardId: string) => {
+      console.log("ran");
+      socket.to(boardId).emit("list-created", list);
+    });
+
+    socket.on("join-board", (boardId: string) => {
+      socket.join(boardId);
+    });
+  });
 }
 
 start();
